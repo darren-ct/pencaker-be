7896ab9e14911b8710d5d4ab09a6d3322d3337d2
ChrisThaDeveloper <onlinesports.id@gmail.com>
2022-08-07 19:13:36 +0700

    Commited

--- config/connect.js
+++ config/connect.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("hackathon","root","mysql.mik.158",{
-    host:"https://pencaker-be.herokuapp.com",
+const sequelize = new Sequelize("hackathon","raja","NDiwno129*&e",{
+    host:"103.172.205.100",
     port:3306,
     dialect:"mysql"
 });
--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.use("/",require("./routes/user"));
 
 
 // listen
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT || 3000,()=>{
      console.log("Connected")
 });
