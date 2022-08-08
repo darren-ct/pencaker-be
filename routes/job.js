26fb285d0ae8c57b5f90e106693e9a3b665e63ac
mikicey <93717921+mikicey@users.noreply.github.com>
2022-08-08 18:38:34 +0700

    Update job.js

--- routes/job.js
+++ routes/job.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const verifyJWT = require("../middlewares/verifyJWT")
 
 
-const { getJobs, getJob,getMyJobs,postJob,applyJob , getAppliedJobs } = require("../controllers/job");
+const { getJobs, getJob, getMyJob, getMyJobs, postJob, applyJob , getAppliedJobs } = require("../controllers/job");
 
 router.get("/job/applied",verifyJWT , getAppliedJobs);
 router.get("/jobs", getJobs);
 router.get("/job/:id", getJob);
 router.get("/myjobs",verifyJWT,getMyJobs)
+router.get("/myjob/:id",verifyJWT,getMyJob);
 router.post("/job", verifyJWT , postJob);
 router.post("/job/apply", verifyJWT , applyJob);
 
