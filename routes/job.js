d5ed8aed73e0d8694058e60863e97459bfd63b2f
mikicey <93717921+mikicey@users.noreply.github.com>
2022-08-08 10:50:09 +0700

    Update job.js

--- routes/job.js
+++ routes/job.js
@@ -5,13 +5,13 @@ const verifyJWT = require("../middlewares/verifyJWT")
 
 const { getJobs, getJob,getMyJobs,postJob,applyJob , getAppliedJobs } = require("../controllers/job");
 
-
+router.get("/job/applied",verifyJWT , getAppliedJobs);
 router.get("/jobs", getJobs);
 router.get("/job/:id", getJob);
 router.get("/myjobs",verifyJWT,getMyJobs)
 router.post("/job", verifyJWT , postJob);
 router.post("/job/apply", verifyJWT , applyJob);
-router.get("/job/applied",verifyJWT , getAppliedJobs);
+
 
 
 
