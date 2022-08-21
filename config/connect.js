441106c46db4487bbc48aafb8b75c3a6d9a36d91
Darren Christian <93717921+darren-ct@users.noreply.github.com>
2022-08-21 18:43:26 +0700

    Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("hackathon","raja","NDiwno129*&e",{
-    host:"103.172.205.100",
+const sequelize = new Sequelize("hackathon","chris","NDiwno129*&e",{
+    host:"103.181.143.80",
     port:3306,
     dialect:"mysql"
 });
